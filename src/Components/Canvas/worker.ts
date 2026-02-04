38bdb0ec2ccdd50b76203c851706639ce5bbc91c
mehraj <its.mehraj@proton.me>
2026-02-04 19:50:31 +0100

    add color to mandelbrot set

--- src/Components/Canvas/worker.ts
+++ src/Components/Canvas/worker.ts
@@ -1,4 +1,12 @@
-self.onmessage = (message) => {
+type Work = {
+  tile: { x: number; y: number; width: number; height: number };
+  x0: number;
+  y0: number;
+  perPixel: number;
+  maxIterations: number;
+};
+
+self.onmessage = (message: MessageEvent<Work>) => {
   const { tile, x0, y0, perPixel, maxIterations } = message.data;
   const { height, width } = tile;
 
@@ -14,19 +22,22 @@ self.onmessage = (message) => {
   // the inner loop does the mandelbrot iteration
   for (let row = 0, y = y0; row < height; row++, y += perPixel) {
     for (let column = 0, x = x0; column < width; column++, x += perPixel) {
-      let n;
+      let n: number;
       let r = x,
         i = y0;
       for (n = 0; n < maxIterations; n++) {
         const rr = r * r,
           ii = i * i;
-        if (rr + ii > 4) {
+        if (rr + ii > 3) {
           break;
         }
         i = 2 * r * i + y0;
         r = rr - ii + x;
       }
       iterations[index++] = n;
+      // iterations[index + 1] = 22; // 0x00ff00; // green channel
+      // iterations[index + 2] = 123; // blue channel
+      // index++; // skip alpha channel
       if (n > max) max = n;
       if (n < min) min = n;
     }
